0c4266873dd0d7a5b4b15534b20ed6031d645e99
CAPOTORTO\Patrizia <patriziacapotorto1@gmail.com>
2017-03-08 18:09:23 +0100

    creazione funzioni per il db

--- server/users/index.js
+++ server/users/index.js
@@ -6,4 +6,16 @@ var Users = require('./users.controller.js');
 //LISTA UTENTI
 router.get('/', Users.getUsers);
 
+//CREAZIONE UTENTI
+router.post('/', Users.createUser);
+
+//DETTAGLIO UTENTE
+router.get('/:id', Users.detailUser);
+
+//ELIMINA UTENTE
+router.delete('/:id', Users.deleteUser);
+
+//AGGIORNO UTENTE
+router.put('/:id', Users.updateUser);
+
 module.exports = router;
--- server/users/users.controller.js
+++ server/users/users.controller.js
@@ -1,10 +1,33 @@
 module.exports = (function (){
 
   var getUsers = function(req,res){
-    res.send('utenti');
+    res.send('lista utenti');
   };
+
+  var createUser = function(req,res){
+    res.send('Creazione utente');
+  };
+
+  var detailUser = function(req,res){
+    var id = req.params.id;
+    res.send('Dettaglio utente con id: '+id);
+  };
+
+  var deleteUser = function(req,res){
+    var id = req.params.id;
+    res.send('Eliminazione utente con id: '+id);
+  };
+  var updateUser = function(req,res){
+    var id = req.params.id;
+    res.send('Aggiornato utente con id: '+id);
+  };
+
   return {
-    getUsers: getUsers
+    getUsers: getUsers,
+    createUser:  createUser,
+    detailUser: detailUser,
+    deleteUser: deleteUser,
+    updateUser: updateUser
   }
-  
+
 })();
