e49c2bf8406b42e3d4df39477b63b2dfce0658b1
CAPOTORTO\Patrizia <patriziacapotorto1@gmail.com>
2017-03-08 16:13:59 +0100

    creazione database mongo

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express(); //lancio express
 var path = require('path');
 
-// var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 // //connessione al database
-// mongoose.connect('mongodb://admin:admin@ds123400.mlab.com:23400/artoo', function(err){
-//   if(err){console.log(err);}else{console.log("connesso");}
-// });
+mongoose.connect('mongodb://admin:admin@ds123410.mlab.com:23410/artoobook', function(err){
+  if(err){console.log(err);}else{console.log("connesso al db");}
+});
 
 //collegamenti per bootstrap
 // app.use('/jquery',express.static(path.join(__dirname,"..","node_modules","jquery","dist")));
