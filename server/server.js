9371c19a15f980b8d821c4e75153914da3ca9420
CAPOTORTO\Patrizia <patriziacapotorto1@gmail.com>
2017-03-08 15:52:38 +0100

    caricamento index.html

--- server/server.js
+++ server/server.js
@@ -8,13 +8,15 @@ var path = require('path');
 //   if(err){console.log(err);}else{console.log("connesso");}
 // });
 
+//collegamenti per bootstrap
+// app.use('/jquery',express.static(path.join(__dirname,"..","node_modules","jquery","dist")));
+// app.use('/bootstrap',express.static(path.join(__dirname,"..","node_modules","bootstrap","dist")));
 
 
 
-
-// app.get('/', function(req,res){
-//   res.send("ciao");
-// });
+app.get('/', function(req,res){
+  res.sendFile(path.join(__dirname,"..","client","index.html"));
+});
 
 app.listen(3002, function(){
   console.log("server in ascolto su http://localhost:3002");
